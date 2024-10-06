5fbc7ad0ccf0aec54671e2caf49304e4fa587b54
habeginner <dreamgktjdejr@gmail.com>
2024-10-06 12:05:28 +0900

    ImageEditPageDetail

---
+++ src/components/ImageEditorComponent/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+      <header className="bg-gray-800 text-white p-4 text-center">
+        <h1 className="text-xl font-bold">이미지 편집 화면</h1>
+      </header>
+    );
+  };
+  
+  export default Header;
+  
