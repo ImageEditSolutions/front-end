60445a836d446c63bd8ebb767e54e878b30e258b
habeginner <dreamgktjdejr@gmail.com>
2024-10-06 19:49:12 +0900

    불필요한 이미지 제거

--- src/components/Main/ImageUploadBox.jsx
+++ src/components/Main/ImageUploadBox.jsx
@@ -50,21 +50,7 @@ const ImageUploadBox = () => {
         />
       ) : (
         <div className="text-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-10 w-10 mx-auto mb-2 text-gray-500"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth="2"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3 16l4-4m0 0l4 4m-4-4v12m13-12h-3.586l-1 1H8m0 0l1 1m3-1l4-4m0 0l4 4"
-            />
-          </svg>
-          <p className="text-gray-400">이미지를 추가해주세요</p>
+          <p className="text-gray-400">+이미지를 추가해주세요</p>
         </div>
       )}
     </div>
