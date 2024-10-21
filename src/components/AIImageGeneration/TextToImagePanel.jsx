67581b3daf6eb3d37d6099f466290f75fdf8625f
habeginner <dreamgktjdejr@gmail.com>
2024-10-21 17:21:16 +0900

    style:  UI개선 : 이미지 생성하기 옆 svg이미지 위치 오른쪽 고정

--- src/components/AIImageGeneration/TextToImagePanel.jsx
+++ src/components/AIImageGeneration/TextToImagePanel.jsx
@@ -8,22 +8,16 @@ export default function TextToImagePanel({ submitPrompt }) {
 
   const handleChange = (event) => {
     const isFilled = !!event.target.value;
-    if (!isFilled) {
-      setIsInputFilled(false);
-    } else {
-      if (isFilled !== isInputFilled) {
-        setIsInputFilled(true);
-      }
-    }
+    setIsInputFilled(isFilled);
     inputRef.current = event.target.value;
-  }
+  };
 
   const handleClick = () => {
     if (!isInputFilled) {
       alert("내용을 입력해주세요.");
     }
     submitPrompt(inputRef.current);
-  }
+  };
 
   return (
     <>
@@ -32,9 +26,9 @@ export default function TextToImagePanel({ submitPrompt }) {
         <div className={`${isInputFilled ? 'h-[286px]' : 'h-[73px]'} bg-gray-800 rounded-lg flex justify-between w-auto px-5 mb-[23px] flex-initial`}>
           <img className="inline w-[27px] h-[27px] mt-[22px] flex-initial" src="/src/assets/prompt-img.svg" alt="prompt-img" />
           <textarea ref={inputRef} onChange={handleChange} className="flex-1 h-[73px] w-max bg-gray-800 resize-none py-[22px] mx-[27px] outline-0" placeholder="떠오른 영감을 자유롭게 작성해주세요."></textarea>
-          <button onClick={handleClick} className="flex-initial text-neutral-950 bg-blue-200 rounded-2xl min-w-48 w-1/6 h-[50px] px-7 py-3 my-auto">
-            이미지 생성하기
-            <img className="inline pl-1 w-max h-max" src="/src/assets/checkmark.svg" alt="checkmark" />
+          <button onClick={handleClick} className="flex-initial flex items-center justify-between text-neutral-950 bg-blue-200 rounded-2xl min-w-48 w-1/6 h-[50px] px-7 py-3 my-auto">
+            <span>이미지 생성하기</span>
+            <img className="inline w-[20px] h-[20px] ml-2" src="/src/assets/checkmark.svg" alt="checkmark" /> {/* 체크마크를 오른쪽에 배치 */}
           </button>
         </div>
         <div className="flex">
@@ -45,7 +39,7 @@ export default function TextToImagePanel({ submitPrompt }) {
         </div>
       </section>
     </>
-  )
+  );
 }
 
 // PropTypes를 이용한 props 검증
