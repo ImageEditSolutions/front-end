4c6557b3b8539e3b878dc34eb66e7e80a76b975d
Chaeyun06 <kilskg@gmail.com>
2024-10-27 14:05:37 +0900

    Feat: theme 파일 따로 관리

---
+++ src/ui/theme/myTheme.js
@@ -0,0 +1,31 @@
+const myTheme = {
+  'common.bi.image': '',
+  'common.bisize.width': '0px',
+  'common.bisize.height': '0px',
+  'common.backgroundColor': '#000000', // 배경색을 검은색으로 유지
+  'common.border': '0px',
+
+  // 메뉴의 텍스트 색상과 폰트 스타일
+  'menu.normalIcon.color': '#FFFFFF',
+  'menu.activeIcon.color': '#FFFFFF',
+  'menu.disabledIcon.color': '#434343',
+  'menu.hoverIcon.color': '#FF0000',
+  'menu.normalLabel.color': '#FFFFFF', // 하얀색 텍스트
+  'menu.normalLabel.fontWeight': 'bold', // 볼드체
+  'menu.activeLabel.color': '#FFFFFF',
+  'menu.activeLabel.fontWeight': 'bold', // 볼드체
+
+  // 서브 메뉴 스타일
+  'submenu.backgroundColor': '#000000',
+  'submenu.partition.color': '#858585',
+  'submenu.normalIcon.color': '#FFFFFF',
+  'submenu.activeIcon.color': '#FFFFFF',
+  'submenu.disabledIcon.color': '#434343',
+  'submenu.hoverIcon.color': '#FF0000',
+  'submenu.normalLabel.color': '#FFFFFF', // 하얀색 텍스트
+  'submenu.normalLabel.fontWeight': 'bold', // 볼드체
+  'submenu.activeLabel.color': '#FFFFFF',
+  'submenu.activeLabel.fontWeight': 'bold', // 볼드체
+};
+
+export default myTheme;
