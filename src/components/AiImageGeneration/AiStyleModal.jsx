760b310ff375cf55b066a3520342f70b556b9779
habeginner <dreamgktjdejr@gmail.com>
2024-10-28 14:49:45 +0900

    Feat: ai 이미지 생성창 스타일 모달 호버 속성 추가

--- src/components/AiImageGeneration/AiStyleModal.jsx
+++ src/components/AiImageGeneration/AiStyleModal.jsx
@@ -1,30 +1,34 @@
 import PropTypes from 'prop-types'; // PropTypes import
+import { useState } from 'react';
 
 // 이미지와 텍스트 데이터를 배열로 관리
 const styles = [
-  { src: "/src/assets/prohibition.svg", alt: "No Style", label: "No style" },
-  { src: "/src/assets/gray.jpeg", alt: "3d-model", label: "3d-model" },
-  { src: "/src/assets/blur.jpeg", alt: "analog-film", label: "analog-film" },
-  { src: "/src/assets/darken.jpeg", alt: "anime", label: "anime" },
-  { src: "/src/assets/sepia.jpeg", alt: "cinematic", label: "cinematic" },
-  { src: "/src/assets/sepia.jpeg", alt: "comic-book", label: "comic-book" },
-  { src: "/src/assets/sepia.jpeg", alt: "digital-art", label: "digital-art" },
-  { src: "/src/assets/sepia.jpeg", alt: "enhance", label: "enhance" },
-  { src: "/src/assets/sepia.jpeg", alt: "fantasy-art", label: "fantasy-art" },
-  { src: "/src/assets/sepia.jpeg", alt: "isometric", label: "isometric" },
-  { src: "/src/assets/sepia.jpeg", alt: "line-art", label: "line-art" },
-  { src: "/src/assets/sepia.jpeg", alt: "low-poly", label: "low-poly" },
-  { src: "/src/assets/sepia.jpeg", alt: "modeling-compound", label: "modeling-compound" },
-  { src: "/src/assets/sepia.jpeg", alt: "neon-punk", label: "neon-punk" },
-  { src: "/src/assets/sepia.jpeg", alt: "origami", label: "origami" },
-  { src: "/src/assets/sepia.jpeg", alt: "pixel-art", label: "pixel-art" },
-  { src: "/src/assets/sepia.jpeg", alt: "photographic", label: "photographic" },
-  { src: "/src/assets/sepia.jpeg", alt: "tile-texture", label: "tile-texture" },
+  { src: "/images/ai/style/Nostyle.svg", alt: "No Style", label: "No style" },
+  { src: "/images/ai/style/style_3D-model.jpg", alt: "3d-model", label: "3d-model" },
+  { src: "/images/ai/style/style_analog-film.jpg", alt: "analog-film", label: "analog-film" },
+  { src: "/images/ai/style/style_anime.jpg", alt: "anime", label: "anime" },
+  { src: "/images/ai/style/style_cineatic.jpg", alt: "cinematic", label: "cinematic" },
+  { src: "/images/ai/style/style_comic-book.jpg", alt: "comic-book", label: "comic-book" },
+  { src: "/images/ai/style/style_digital art.jpg", alt: "digital-art", label: "digital-art" },
+  { src: "/images/ai/style/style_enhance.jpg", alt: "enhance", label: "enhance" },
+  { src: "/images/ai/style/style_fantasy-art.jpg", alt: "fantasy-art", label: "fantasy-art" },
+  { src: "/images/ai/style/style_isometric.jpg", alt: "isometric", label: "isometric" },
+  { src: "/images/ai/style/style_line-art.jpg", alt: "line-art", label: "line-art" },
+  { src: "/images/ai/style/style_low-poly.jpg", alt: "low-poly", label: "low-poly" },
+  { src: "/images/ai/style/style_modeling-compund_tree.jpg", alt: "modeling-compound", label: "modeling-compound" },
+  { src: "/images/ai/style/style_neon-art.jpg", alt: "neon-punk", label: "neon-punk" },
+  { src: "/images/ai/style/style_origami.jpg", alt: "origami", label: "origami" },
+  { src: "/images/ai/style/style_photographic.jpg", alt: "photographic", label: "photographic" },
+  { src: "/images/ai/style/style_picxel-art.jpg", alt: "pixel-art", label: "pixel-art" },
+  { src: "/images/ai/style/style_tile-texture.jpg", alt: "tile-texture", label: "tile-texture" },
 ];
 
 export default function AiStyleModal({ isOpen, onClose, saveImageAttributes }) {
+  const [selectedStyle, setSelectedStyle] = useState(''); // 선택된 스타일 상태
+
   const handleClick = (e) => {
     const styleLabel = e.currentTarget.id;
+    setSelectedStyle(styleLabel); // 선택된 스타일 업데이트
     saveImageAttributes({ style: styleLabel });
   };
 
@@ -40,7 +44,8 @@ export default function AiStyleModal({ isOpen, onClose, saveImageAttributes }) {
               onClick={handleClick}
               id={style.label}
               key={style.label}
-              className="max-w-28 h-24 bg-gray-700 rounded-lg flex flex-col items-center justify-center overflow-hidden relative cursor-pointer"
+              className={`max-w-28 h-24 bg-gray-700 rounded-lg flex flex-col items-center justify-center overflow-hidden relative cursor-pointer 
+                transition ${selectedStyle === style.label ? "border-2 border-sky-400" : "hover:border-2 hover:border-sky-400"}`}
             >
               <img
                 src={style.src}
