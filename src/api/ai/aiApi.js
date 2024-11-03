c490735804e02176b2c448d00513a234d8c11f1d
Chaeyun06 <kilskg@gmail.com>
2024-11-03 15:24:55 +0900

    Fix: api 문제

--- src/api/ai/aiApi.js
+++ src/api/ai/aiApi.js
@@ -1,4 +1,6 @@
 // import axios from 'axios';
+import error from 'eslint-plugin-react/lib/util/error.js';
+
 const apiKey = import.meta.env.VITE_STABILITY_API_KEY;
 
 // let requestCount = 0;
@@ -49,5 +51,42 @@ const requestAIImageGeneration = async ({ text, style, numImages}) => {
   const responseJSON = await response.json();
   return responseJSON.artifacts.map((artifact) => `data:image/png;base64,${artifact.base64}`);
 };
+
+// const handleSubmit = async () => {
+//   if (attachedFile) {
+//     const translatedText = await translatePrompt();
+//     if (translatedText) {
+//       await callStabilityAI(translatedText);
+//     }
+//   } else {
+//     try {
+//
+//     }
+//   } catch (error) {
+//     console.error("이미지 생성 요청 오류: " , error);
+//   }
+// }
+//
+// const translatePrompt = async () => {
+//   try {
+//     const response = await fetch('https://api.stability.ai', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({prompt}),
+//     });
+//
+//     if (!response.ok) throw new Error('번역 오류: ' + response);
+//
+//     const data = await response.json();
+//     const translatedText = data.translation[0].text;
+//     setTranslatedPrompt(translatedText);
+//     return translatedText;
+//   } catch (error) {
+//     console.log("번역 API 요청 오류:", error);
+//     return null;
+//   }
+// }
 
 export default requestAIImageGeneration;
