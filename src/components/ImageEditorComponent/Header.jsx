8452dc78f6e5bf172fef406d2c85b120a6257656
habeginner <dreamgktjdejr@gmail.com>
2024-11-04 19:46:25 +0900

    Refactor: 새로운 프로젝트 버튼 제거

--- src/components/ImageEditorComponent/Header.jsx
+++ src/components/ImageEditorComponent/Header.jsx
@@ -1,30 +1,27 @@
-const   Header = ({ actions, handleModalOpen }) => {
-
-  return (
-    <header className="bg-edit-gray text-white p-4 text-center flex gap-7 m-center rounded-bl-3xl px-8 h-14">
-      <button className="flex h-full gap-2" onClick={actions.CREATE_NEW_PROJECT}>
-        <img className="h-full object-cover" src="/images/edit/new.png" alt="newProject" />
-        <p>새로운 프로젝트</p>
-      </button>
-      <button className="flex h-full gap-2" onClick={actions.LOAD_IMAGE}>
-        <img className="h-full object-cover" src="/images/edit/loadEditableImg.png" alt="loadEditableImg" />
-        <p>편집할 이미지 가져오기</p>
-      </button>
-      <button className="flex h-full gap-2" onClick={actions.DOWNLOAD_IMAGE}>
-        <img className="h-full object-cover" src="/images/edit/download.png" alt="download" />
-        <p>이미지 다운로드</p>
-      </button>
-      <button className="flex h-full gap-2" onClick={() => handleModalOpen('save_project')}>
-        <img className="h-full object-cover" src="/images/edit/save.png" alt="save" />
-        <p>프로젝트 저장하기</p>
-      </button>
-      <button className="flex h-full gap-2" onClick={() => {handleModalOpen('load_project')}}>
-        <img className="h-full object-cover" src="/images/edit/loadProject.png" alt="loadProject" />
-        <p>프로젝트 가져오기</p>
-      </button>
-    </header>
-  );
-};
+const Header = ({ actions, handleModalOpen }) => (
+  <header className="bg-edit-gray text-white p-4 text-center flex gap-7 m-center rounded-bl-3xl px-8 h-14">
+    {/* <button className="flex h-full gap-2" onClick={() => actions.CREATE_NEW_PROJECT()}>
+      <img className="h-full object-cover" src="/images/edit/new.png" alt="newProject" />
+      <p>새로운 프로젝트</p>
+    </button> */}
+    <button className="flex h-full gap-2" onClick={() => actions.LOAD_IMAGE()}>
+      <img className="h-full object-cover" src="/images/edit/loadEditableImg.png" alt="loadEditableImg" />
+      <p>편집할 이미지 가져오기</p>
+    </button>
+    <button className="flex h-full gap-2" onClick={() => actions.DOWNLOAD_IMAGE()}>
+      <img className="h-full object-cover" src="/images/edit/download.png" alt="download" />
+      <p>이미지 다운로드</p>
+    </button>
+    <button className="flex h-full gap-2" onClick={() => handleModalOpen('save_project')}>
+      <img className="h-full object-cover" src="/images/edit/save.png" alt="save" />
+      <p>프로젝트 저장하기</p>
+    </button>
+    <button className="flex h-full gap-2" onClick={() => handleModalOpen('load_project')}>
+      <img className="h-full object-cover" src="/images/edit/loadProject.png" alt="loadProject" />
+      <p>프로젝트 가져오기</p>
+    </button>
+  </header>
+);
 
 export default Header;
 
