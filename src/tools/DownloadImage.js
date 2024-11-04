b7d9a27b96443f43ccd25a27dd42dd1e62b7b5de
Chaeyun06 <kilskg@gmail.com>
2024-11-04 23:13:14 +0900

    Fix: downloadimage 수정 (base64)

--- src/tools/DownloadImage.js
+++ src/tools/DownloadImage.js
@@ -1,3 +1,13 @@
+function base64ToBlob(base64, mimeType) {
+  const byteCharacters = atob(base64);
+  const byteNumbers = new Array(byteCharacters.length);
+  for (let i = 0; i < byteCharacters.length; i++) {
+    byteNumbers[i] = byteCharacters.charCodeAt(i);
+  }
+  const byteArray = new Uint8Array(byteNumbers);
+  return new Blob([byteArray], { type: mimeType });
+}
+
 const DownloadImage = (editor ) => {
   console.log('saveimage 실행');
   const dataURL = editor.toDataURL({
@@ -6,12 +16,18 @@ const DownloadImage = (editor ) => {
     multiplier: 1,  //fixme 원래 10
   });
 
+  const [mimeType, base64Data] = dataURL.split(';base64,');
+// Blob 생성
+  const blob = base64ToBlob(base64Data, mimeType);
+
   // 파일 이름 생성
   // const fileName = originalFileName.replace(/\.[^/.]+$/, "") + "-edit.png";
 
+  const url = URL.createObjectURL(blob);
+  console.log(url);
 
   const link = document.createElement('a');
-  link.href = dataURL;
+  link.href = url;
   link.download = 'edit.png';
   document.body.appendChild(link);
   link.click();
