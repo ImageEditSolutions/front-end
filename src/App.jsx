00792aae3694ccd9a5d72e6a39e17c335e82ac88
Chaeyun06 <kilskg@gmail.com>
2024-11-21 13:11:21 +0900

    Feat: 404 오류 처리 핸들러

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import ImageEditPage from "./pages/ImageEditPage";
 import AiImageGenerationPage from "./pages/AiImageGenerationPage";
+import Page404 from './Page404.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainPage />,  // "/" 경로에선 메인 페이지 렌더링
+    errorElement: <Page404 />,
   },
   {
     path: "/image-edit",
---
+++ src/Page404.jsx
@@ -0,0 +1,9 @@
+const Page404 = () => {
+  return (
+    <>
+      <h1> 404 잘못된 페이지 </h1>
+    </>
+  )
+}
+
+export default Page404;
